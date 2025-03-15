927aa8ef8affcdd706faf07523aeb1dec7424e21
abdul8840 <abdul14941naseer@gmail.com>
2025-03-15 11:22:36 +0530

    solve the image error

--- client/src/components/HomeContent/HomeBanner.jsx
+++ client/src/components/HomeContent/HomeBanner.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Banner1 from '../../assets/images/homeBanner1.jpg';
-import Banner2 from '../../assets/images/gallerybg.jpg';
-import Banner3 from '../../assets/images/homeBanner3.jpg';
+import Banner2 from '../../assets/images/hBanner2.jpg';
+import Banner3 from '../../assets/images/hBanner3.jpg';
 
 const HomeBanner = () => {
   return (
